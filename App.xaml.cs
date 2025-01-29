b3174539de124a9c17aa68e8050195fbddfdb73d
johanneskopf <johanneskopf@gmx.at>
2025-01-29 09:20:00 +0100

    feat: add empty net9 WPF project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sample_DefaultDataGridCellStyle_override
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
